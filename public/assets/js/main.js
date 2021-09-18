ef63477b5aaf2bc5c985d5f6f2324c5f5fbfef3c
Claudia Stein <claudiasantanastein@hotmail.com>
2021-09-18 21:14:14 +0200

    paintUsers

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const arrayContainer = document.querySelector(".js-section");
 //const friend =
 // const clickedFriend = event.currentTarget;
 //
@@ -10,55 +11,54 @@
 // }
 //
 //
-const image = document.querySelector(".js-image");
+// const image = document.querySelector(".js-image");
 //// VARIABLES PARA LA FUNCIÓN PAINTUSERS
-//let allUsers = [];
-//let allUsersImg = [];
-//let allUsersName = [];
-//let allUsersLogin = [];
-//let allUsersCity = [];
+let allUsers = [];
+let allUsersImg = [];
+let allUsersName = [];
+let allUsersLogin = [];
+let allUsersCity = [];
+let allUsersId = [];
 //
 ////FUNCIÓN QUE PINTA LOS DATOS EN LA SECTION
-//function paintUsers() {
-//  let html = "";
-//  for (const info of allUsers) {
-//    allUsersLogin = info.results.login;
-//    allUsersImg = info.results.picture.medium;
-//    allUsersName = info.results.name;
-//    allUsersCity = info.results.location.city;
-//    html += `<img
-//           class="js-image main__section--container--img"
-//           id=""
-//           src="${allUsersImg}"
-//           alt=""
-//         />`;
-//    html += ` <h3 class="js-user main__section--container--text--user">${allUsersLogin}</h3>`;
-//    html += `<h1 class="js-name main__section--container--text--name">${allUsersName}</h1>`;
-//    html += `<h3 class="js-city main__section--container--text--city">${allUsersCity}</h3>`;
-//  }
-//}
-//--FUNCIÓN DEL THEN QUE BUSCA LOS DATOS DE LA API Y LOS DEVUELVE--
-//const convertToJS = (response) => {
-//   return response.json();
-// };
-
-console.log("antes");
-
-function renderToHTML(data) {
-  console.log(data);
+function paintUsers() {
+  let html = "";
+  for (const info of allUsers) {
+    allUsersId = info.id;
+    allUsersLogin = info.login;
+    allUsersImg = info.picture.medium;
+    allUsersName = info.name;
+    allUsersCity = info.location.city;
+    html += ` <li class="js-container main__section--container" id="${allUsersId}">`;
+    html += `<img
+           class="js-image main__section--container--img"
+           id=""
+           src="${allUsersImg}"
+           alt=""
+         />`;
+    html += ` <h3 class="js-user main__section--container--text--user">${allUsersLogin}</h3>`;
+    html += `<h1 class="js-name main__section--container--text--name">${allUsersName}</h1>`;
+    html += `<h3 class="js-city main__section--container--text--city">${allUsersCity}</h3>`;
+  }
+  arrayContainer.innerHTML = html;
 }
+//--FUNCIÓN DEL THEN QUE BUSCA LOS DATOS DE LA API Y LOS DEVUELVE--
+const convertToJS = (response) => {
+  return response.json();
+};
 
 //
 
 fetch("https://randomuser.me/api/?results=10")
   .then(
-    (response) => response.json()
-    //SI LA FUNCIÓN SOLO TIENE UN PARÁMETRO SE LE PUEDE QUITAR LOS PARÉNTESIS
-    //SI EL RETURN SOLO TIENE UNA LÍNEA, SE PUEDEN QUITAR LLAVES Y EL RETURN
+    (response) =>
+      //SI LA FUNCIÓN SOLO TIENE UN PARÁMETRO SE LE PUEDE QUITAR LOS PARÉNTESIS
+      response.json() //SI EL RETURN SOLO TIENE UNA LÍNEA, SE PUEDEN QUITAR LLAVES Y EL RETURN
   )
 
   .then((data) => {
     allUsers = data;
+    paintUsers();
   });
 
 //# sourceMappingURL=main.js.map
